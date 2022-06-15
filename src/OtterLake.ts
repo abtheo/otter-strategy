718cea91de7d416a4501582b577722aa1fdd5d39
Po-Ying Chen <poying.me@gmail.com>
2022-06-15 22:56:50 +0800

    fix(OtterLake): fix syntax

--- src/OtterLake.ts
+++ src/OtterLake.ts
@@ -7,7 +7,7 @@ enum TERM_SETTING {
   LOCK_PERIOD = 2,
 }
 
-export function handleTermAdded(event: TermAdded) {
+export function handleTermAdded(event: TermAdded): void {
   const term = new Term(event.params.note.toHex());
   term.enabled = true;
   term.note = event.params.note;
@@ -17,7 +17,7 @@ export function handleTermAdded(event: TermAdded) {
   term.save();
 }
 
-export function handleTermUpdated(event: TermUpdated) {
+export function handleTermUpdated(event: TermUpdated): void {
   const term = Term.load(event.params.note.toHex())
   if (!term) {
     return;
@@ -34,11 +34,11 @@ export function handleTermUpdated(event: TermUpdated) {
   term.save();
 }
 
-export function handleTermRemoved(event: TermRemoved) {
+export function handleTermRemoved(event: TermRemoved): void {
   store.remove('Term', event.params.note.toHex());
 }
 
-export function handleTermDisabled(event: TermDisabled) {
+export function handleTermDisabled(event: TermDisabled): void {
   const term = Term.load(event.params.note.toHex())
   if (!term) {
     return;
@@ -48,9 +48,9 @@ export function handleTermDisabled(event: TermDisabled) {
   term.save();
 }
 
-export function handleLocked(event: Locked) {
+export function handleLocked(event: Locked): void {
   const id = event.params.tokenId.toHex();
-  let token = NoteToken.load(id) ?? new NoteToken(id);
+  let token = (NoteToken.load(id) || new NoteToken(id)) as NoteToken;
   token.tokenId = event.params.tokenId;
   token.note = event.params.note;
   token.amount = event.params.amount;

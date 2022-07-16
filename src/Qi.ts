ba3ef91fbbc80c556f70d82337750935c704ba1b
abtheo <c.theo.kent@gmail.com>
2022-07-16 17:08:26 +0200

    Remove APY calculation

--- src/Qi.ts
+++ src/Qi.ts
@@ -13,51 +13,51 @@ import {
 import { toDecimal } from './utils/Decimals'
 import { getQiUsdRate } from './utils/Price'
 
-export function handleQiTransfer(event: TransferEvent): void {
-  if (
-    (event.params.from == UNI_MAI_USDC_QI_INVESTMENT_PAIR || event.params.from == UNI_QI_WMATIC_INVESTMENT_PAIR) &&
-    event.params.to == TREASURY_ADDRESS
-  ) {
-    log.debug('QiDaoInvestmentHarvestTransfer {}, from: {}, to: {}', [
-      event.transaction.hash.toHexString(),
-      event.params.from.toHexString(),
-      event.params.to.toHexString(),
-    ])
-    let transaction = loadOrCreateTransaction(event.transaction, event.block)
-    let entity = new Transfer(transaction.id)
-    entity.transaction = transaction.id
-    entity.timestamp = transaction.timestamp
-    entity.from = event.params.from
-    entity.to = event.params.to
-    entity.value = event.params.value
-    entity.save()
+// export function handleQiTransfer(event: TransferEvent): void {
+//   if (
+//     (event.params.from == UNI_MAI_USDC_QI_INVESTMENT_PAIR || event.params.from == UNI_QI_WMATIC_INVESTMENT_PAIR) &&
+//     event.params.to == TREASURY_ADDRESS
+//   ) {
+//     log.debug('QiDaoInvestmentHarvestTransfer {}, from: {}, to: {}', [
+//       event.transaction.hash.toHexString(),
+//       event.params.from.toHexString(),
+//       event.params.to.toHexString(),
+//     ])
+//     let transaction = loadOrCreateTransaction(event.transaction, event.block)
+//     let entity = new Transfer(transaction.id)
+//     entity.transaction = transaction.id
+//     entity.timestamp = transaction.timestamp
+//     entity.from = event.params.from
+//     entity.to = event.params.to
+//     entity.value = event.params.value
+//     entity.save()
 
-    //Pass entity to TreasuryRevenue
-    updateTreasuryRevenueQiTransfer(entity)
-  }
-  if (event.params.from == QI_BRIBE_REWARDS && event.params.to == OTTER_DEPLOYER) {
-    log.debug('QiDao Bribe Recieved {} for {} Qi, from: {}, to: {}', [
-      event.transaction.hash.toHexString(),
-      toDecimal(event.transaction.value, 18).toString(),
-      event.params.from.toHexString(),
-      event.params.to.toHexString(),
-    ])
-    let transaction = loadOrCreateTransaction(event.transaction, event.block)
-    let entity = new Transfer(transaction.id)
-    entity.transaction = transaction.id
-    entity.timestamp = transaction.timestamp
-    entity.from = event.params.from
-    entity.to = event.params.to
-    entity.value = event.params.value
-    entity.save()
+//     //Pass entity to TreasuryRevenue
+//     updateTreasuryRevenueQiTransfer(entity)
+//   }
+//   if (event.params.from == QI_BRIBE_REWARDS && event.params.to == OTTER_DEPLOYER) {
+//     log.debug('QiDao Bribe Recieved {} for {} Qi, from: {}, to: {}', [
+//       event.transaction.hash.toHexString(),
+//       toDecimal(event.transaction.value, 18).toString(),
+//       event.params.from.toHexString(),
+//       event.params.to.toHexString(),
+//     ])
+//     let transaction = loadOrCreateTransaction(event.transaction, event.block)
+//     let entity = new Transfer(transaction.id)
+//     entity.transaction = transaction.id
+//     entity.timestamp = transaction.timestamp
+//     entity.from = event.params.from
+//     entity.to = event.params.to
+//     entity.value = event.params.value
+//     entity.save()
 
-    //Pass entity to TreasuryRevenue
-    updateTreasuryRevenueQiTransfer(entity)
+//     //Pass entity to TreasuryRevenue
+//     updateTreasuryRevenueQiTransfer(entity)
 
-    //Count for QiBribes
-    let bribes = loadOrCreateTotalBribeRewardsSingleton()
-    let qiMarketValue = getQiUsdRate().times(toDecimal(event.params.value, 18))
-    bribes.qiBribeRewardsMarketValue = bribes.qiBribeRewardsMarketValue.plus(qiMarketValue)
-    bribes.save()
-  }
-}
+//     //Count for QiBribes
+//     let bribes = loadOrCreateTotalBribeRewardsSingleton()
+//     let qiMarketValue = getQiUsdRate().times(toDecimal(event.params.value, 18))
+//     bribes.qiBribeRewardsMarketValue = bribes.qiBribeRewardsMarketValue.plus(qiMarketValue)
+//     bribes.save()
+//   }
+// }

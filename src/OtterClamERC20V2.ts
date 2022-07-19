71c998275462da8282663a61c2313ad9f17ebbdf
abtheo <c.theo.kent@gmail.com>
2022-07-19 13:23:50 +0200

    fix return type

--- src/OtterClamERC20V2.ts
+++ src/OtterClamERC20V2.ts
@@ -3,12 +3,13 @@ import { loadOrCreateTotalBurnedClamSingleton } from './utils/Burned'
 import { toDecimal } from './utils/Decimals'
 import { getClamUsdRate } from './utils/Price'
 
-export function handleBurn(burnCall: BurnCall) {
+export function handleBurn(burnCall: BurnCall): void {
   let burns = loadOrCreateTotalBurnedClamSingleton()
 
   burns.burnedClam = burns.burnedClam.plus(toDecimal(burnCall.inputs.amount, 9))
   burns.burnedValueUsd = burns.burnedValueUsd.plus(
     toDecimal(burnCall.inputs.amount, 9).times(getClamUsdRate(burnCall.block.number)),
   )
+
   burns.save()
 }

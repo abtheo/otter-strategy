d8a0b653ed340653de179abe1884b158a410364c
abtheo <c.theo.kent@gmail.com>
2022-07-22 10:18:14 +0200

    fix cumulative value payouts

--- src/OtterRewardManager.ts
+++ src/OtterRewardManager.ts
@@ -1,26 +1,23 @@
 import { Payout } from '../generated/OtterRewardManager/OtterRewardManager'
 import { PearlBank } from '../generated/OtterRewardManager/PearlBank'
 import { loadOrCreatePearlBankMetric } from './utils/PearlBankMetric'
-import { CLAM_PLUS, PEARL_BANK } from './utils/Constants'
+import { PEARL_BANK } from './utils/Constants'
 import { toDecimal } from './utils/Decimals'
 import { loadCumulativeValues } from './utils/CumulativeValues'
 import { getClamUsdRate } from './utils/Price'
-import { loadOrCreateTransaction } from './utils/Transactions'
 import { BigDecimal } from '@graphprotocol/graph-ts'
-import { ClamPlus } from '../generated/OtterRewardManager/ClamPlus'
 
 export function handlePayout(payout: Payout): void {
   let metric = loadOrCreatePearlBankMetric(payout.block.timestamp)
   let clamPrice = getClamUsdRate(payout.block.number)
-  let clamPondStakedClam = toDecimal(ClamPlus.bind(CLAM_PLUS).totalSupply(), 9)
   let pearlBankStakedClam = toDecimal(PearlBank.bind(PEARL_BANK).totalSupply(), 9)
 
-  let stakedUsd = clamPondStakedClam.plus(pearlBankStakedClam).times(clamPrice)
+  let stakedUsd = pearlBankStakedClam.times(clamPrice)
   let payoutValue = toDecimal(payout.params.totalUsdPlus, 6)
 
   // update cumulative values
   let cumulativeValues = loadCumulativeValues()
-  let newTotalPaid = cumulativeValues.rewardPayoutMarketValue.plus(metric.payoutMarketValue)
+  let newTotalPaid = cumulativeValues.rewardPayoutMarketValue.plus(payoutValue)
   cumulativeValues.rewardPayoutMarketValue = newTotalPaid
   cumulativeValues.save()
 

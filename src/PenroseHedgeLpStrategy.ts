cb46ec3e3417a44a137cc5d5fa0da7fc2ca741a2
abtheo <c.theo.kent@gmail.com>
2022-09-01 12:21:12 +0200

    Add ETS strategy

---
+++ src/PenroseHedgeLpStrategy.ts
@@ -0,0 +1,26 @@
+import { BigDecimal } from '@graphprotocol/graph-ts'
+import { ERC20 } from '../generated/OtterClamERC20V2/ERC20'
+import { ClaimRewardToken as ClaimRewardTokenEvent } from '../generated/PenroseHedgeLpStrategy/PenroseHedgeLpStrategy'
+import { ClaimReward } from '../generated/schema'
+import { DYST_ERC20, PEN_ERC20, QI_ERC20 } from './utils/Constants'
+import { toDecimal } from './utils/Decimals'
+import { loadOrCreateTransaction } from './utils/Transactions'
+import { updateTreasuryRevenueDystRewardPaid, updateTreasuryRevenuePenRewardPaid } from './utils/TreasuryRevenue'
+
+export function handleClaimReward(event: ClaimRewardTokenEvent): void {
+  let transaction = loadOrCreateTransaction(event.transaction, event.block)
+  let claim = new ClaimReward(transaction.id)
+  claim.transaction = transaction.id
+  claim.timestamp = transaction.timestamp
+  claim.amountUsd = toDecimal(event.params.usdcAmount, 6) //Claim in USDC (6 decimals)
+  claim.amountToken = toDecimal(event.params.amount, ERC20.bind(event.params.token).decimals())
+  claim.token = event.params.token
+  claim.save()
+
+  if (event.params.token == PEN_ERC20) {
+    updateTreasuryRevenuePenRewardPaid(transaction, event.params.amount)
+  }
+  if (event.params.token == DYST_ERC20) {
+    updateTreasuryRevenueDystRewardPaid(transaction, event.params.amount)
+  }
+}

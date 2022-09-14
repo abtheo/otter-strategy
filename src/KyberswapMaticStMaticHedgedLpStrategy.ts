519221f3cf5d82355e13f50e620d0fa6613e0aa4
abtheo <c.theo.kent@gmail.com>
2022-09-14 11:32:42 +0200

    include in treasury revenues

---
+++ src/KyberswapMaticStMaticHedgedLpStrategy.ts
@@ -0,0 +1,25 @@
+import { ClaimRewardToken as ClaimRewardTokenEvent } from '../generated/KyberswapMaticStMaticHedgedLpStrategy/KyberswapMaticStMaticHedgedLpStrategy'
+import { ERC20 } from '../generated/OtterClamERC20V2/ERC20'
+import { ClaimReward } from '../generated/schema'
+import { KNC_ERC20, LDO_ERC20 } from './utils/Constants'
+import { toDecimal } from './utils/Decimals'
+import { loadOrCreateTransaction } from './utils/Transactions'
+import { updateTreasuryRevenueClaimKncReward, updateTreasuryRevenueClaimLdoReward } from './utils/TreasuryRevenue'
+
+export function handleClaimReward(event: ClaimRewardTokenEvent): void {
+  let transaction = loadOrCreateTransaction(event.transaction, event.block)
+  let claim = new ClaimReward(transaction.id)
+  claim.transaction = transaction.id
+  claim.timestamp = transaction.timestamp
+  claim.amountUsd = toDecimal(event.params.usdcAmount, 6) //Claim in USDC (6 decimals)
+  claim.amountToken = toDecimal(event.params.amount, ERC20.bind(event.params.token).decimals())
+  claim.token = event.params.token
+  claim.save()
+
+  if (event.params.token == KNC_ERC20) {
+    updateTreasuryRevenueClaimKncReward(event.block.number, claim)
+  }
+  if (event.params.token == LDO_ERC20) {
+    updateTreasuryRevenueClaimLdoReward(event.block.number, claim)
+  }
+}

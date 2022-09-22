507fd783a457219166019093ef47cdc8ff266904
abtheo <c.theo.kent@gmail.com>
2022-09-22 11:06:37 +0200

    Use PayoutReward revenue

--- src/UniV3UsdcMaiStrategy.ts
+++ src/UniV3UsdcMaiStrategy.ts
@@ -1,17 +1,17 @@
-import { ClaimReward as ClaimRewardEvent } from '../generated/UniV3UsdcMaiStrategy/UniV3UsdcMaiStrategy'
+import { PayoutReward as PayoutRewardEvent } from '../generated/UniV3UsdcMaiStrategy/UniV3UsdcMaiStrategy'
 import { ClaimReward } from '../generated/schema'
 import { toDecimal } from './utils/Decimals'
 import { loadOrCreateTransaction } from './utils/Transactions'
 import { updateTreasuryRevenueClaimUsdcReward } from './utils/TreasuryRevenue'
 import { USDC_ERC20 } from './utils/Constants'
 
-export function handleClaimReward(event: ClaimRewardEvent): void {
+export function handlePayoutReward(event: PayoutRewardEvent): void {
   let transaction = loadOrCreateTransaction(event.transaction, event.block)
   let claim = new ClaimReward(transaction.id)
   claim.transaction = transaction.id
   claim.timestamp = transaction.timestamp
-  claim.amountUsd = toDecimal(event.params.amount, 6) //Claim in USDC (6 decimals)
-  claim.amountToken = toDecimal(event.params.amount, 6)
+  claim.amountUsd = toDecimal(event.params.revenue, 6) //Claim in USDC (6 decimals)
+  claim.amountToken = toDecimal(event.params.revenue, 6)
   claim.token = USDC_ERC20
   claim.save()
 

3aed08d98a3182fc8456d0f52dbd8ef935f995da
abtheo <c.theo.kent@gmail.com>
2022-10-03 14:25:21 +0200

    Track PayoutReward for univ3-hedged-matic

--- src/UniV3MaticUsdcHedgedLpStrategy.ts
+++ src/UniV3MaticUsdcHedgedLpStrategy.ts
@@ -1,6 +1,9 @@
-import { ClaimRewardToken as ClaimRewardTokenEvent } from '../generated/UniV3MaticUsdcHedgedLpStrategy/IStrategy'
+import {
+  ClaimRewardToken as ClaimRewardTokenEvent,
+  PayoutReward as PayoutRewardEvent,
+} from '../generated/UniV3MaticUsdcHedgedLpStrategy/IStrategy'
 import { ERC20 } from '../generated/OtterClamERC20V2/ERC20'
-import { ClaimReward } from '../generated/schema'
+import { ClaimReward, PayoutReward } from '../generated/schema'
 import { MATIC_ERC20, USDC_ERC20 } from './utils/Constants'
 import { toDecimal } from './utils/Decimals'
 import { loadOrCreateTransaction } from './utils/Transactions'
@@ -26,4 +29,18 @@ export function handleClaimRewardToken(event: ClaimRewardTokenEvent): void {
 
   let investment = new UniV3HedgedMaticUsdcInvestment(transaction)
   investment.addRevenue(claim)
+}
+
+export function handlePayoutReward(event: PayoutRewardEvent): void {
+  let transaction = loadOrCreateTransaction(event.transaction, event.block)
+  let investment = new UniV3HedgedMaticUsdcInvestment(transaction)
+
+  let payout = new PayoutReward(`${investment.strategy}_${transaction.id}`)
+  payout.netAssetValue = toDecimal(event.params.nav, 6)
+  payout.revenue = toDecimal(event.params.revenue, 6)
+  payout.payout = toDecimal(event.params.payout, 6)
+  payout.transactionHash = transaction.id
+  payout.save()
+
+  investment.calculateNetProfit(payout)
 }
